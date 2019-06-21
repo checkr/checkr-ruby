8469b9e71693385af4bd4d3a538d566519e6abe1
Roman Tymchyk <roman.tymchyk@careguide.com>
2019-06-20 22:37:34 -0400

    All adverse actions through report endpoint

--- lib/checkr/adverse_action.rb
+++ lib/checkr/adverse_action.rb
@@ -8,7 +8,7 @@ module Checkr
     attribute :individualized_assessment_engaged
     attribute :adverse_items, APIList.constructor(:AdverseItem)
 
-    api_class_method :all, :get, :constructor => APIList.constructor(:AdverseAction)
+    api_class_method :all, :get, '/v1/reports/:report_id/adverse_actions', :constructor => APIList.constructor(:AdverseAction)
     api_class_method :create, :post, '/v1/reports/:report_id/adverse_actions'
     api_class_method :retrieve, :get, ':path/:id', :arguments => [:id]
     api_class_method :cancel, :delete, ':path/:id', :arguments => [:id]
